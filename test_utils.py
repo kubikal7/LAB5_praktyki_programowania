a09bbc2141ee23c9e29cdb4d30e4d4695d52ac90
Bartek <bartek.kuk@o2.pl>
2025-05-07 16:14:12 +0200

    binary function

--- test_utils.py
+++ test_utils.py
@@ -32,3 +32,8 @@ def test_multiply(a, b, expected):
 def test_divide(a, b, expected):
     result = utils.divide(a, b)
     assert result == expected
+
+@pytest.mark.parametrize("a, expected", [(7, '111'), (99, '1100011'), (41, '101001')])
+def binary(a, expected):
+    result = utils.binary(a)
+    assert result == expected
